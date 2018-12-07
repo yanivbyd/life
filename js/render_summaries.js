056aeb277571c91fa9b5e8615d5a81ec8d2e44d2
Yaniv Ben-Yehuda <yanivby@artwaresoft.com>
2018-12-07 18:12:09 +0200

    Show vegetation in render summary

--- js/render_summaries.js
+++ js/render_summaries.js
@@ -14,7 +14,7 @@ SamplingGroup.prototype.sample = function(val)
 SamplingGroup.prototype.toString = function()
 {
     var out = [];
-    out.push("count=" + this.count);
+    out.push("count=" + shortNumber(this.count));
     if (this.sum) {
         var avg = this.sum / this.count;
         out.push("avg="+avg.toFixed(2));
@@ -25,6 +25,7 @@ SamplingGroup.prototype.toString = function()
 
 function renderSummary(textbox, world)
 {
+    var vegetation = new SamplingGroup(global_world_params.veg.maxAmount);
     var creatures = [];
     for (var i=0;i<global_world_params.creatures.length;i++) {
         creatures.push(new SamplingGroup(global_world_params.creature.maxHealth));
@@ -33,12 +34,14 @@ function renderSummary(textbox, world)
     for (var row=0;row<world.size;row++)
         for (var col=0;col<world.size;col++) {
             var cell = world.matrix[row][col];
+            vegetation.sample(cell.vegetation);
             if (cell.creature) creatures[cell.creature.type].sample(cell.creature.health);
         }
 
     var text = "";
     for (var i=0;i<global_world_params.creatures.length;i++) {
-        text += "creature ["+(i+1)+"]: "+ creatures[i].toString()+"\n";
+        text += global_world_params.creatures[i].name + ": "+ creatures[i].toString()+"\n";
     }
+    text += "vegetation: " + vegetation.toString() + "\n";
     $(textbox).text(text);
 }
--- js/utils.js
+++ js/utils.js
@@ -14,3 +14,15 @@ function randomArrayItem(arr)
     if (arr.length == 1) return arr[0];
     return arr[randomInt(arr.length)];
 }
+
+function numberWithCommas(num)
+{
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function shortNumber(num)
+{
+    if (num>10*1000*1000) return Math.floor(num/1000000) + "m";
+    if (num>10*1000) return Math.floor(num/1000) + "k";
+    return num;
+}
