a0a535915cc255adaf7153c114de5aa0c85e9f4f
Yaniv Ben-Yehuda <yanivby@artwaresoft.com>
2018-12-07 12:58:48 +0200

    Small render fixes

--- js/render_summaries.js
+++ js/render_summaries.js
@@ -2,17 +2,17 @@ function renderSummary(textbox, world)
 {
     var creaturesCount = [];
     for (var i=0;i<global_world_params.creatures.length;i++)
-        creaturesCount.push(0);
+        creaturesCount.push({ count: 0 });
 
     for (var row=0;row<world.size;row++)
         for (var col=0;col<world.size;col++) {
             var cell = world.matrix[row][col];
-            if (cell.creature) creaturesCount[cell.creature.type]++;
+            if (cell.creature) creaturesCount[cell.creature.type].count++;
         }
 
     var text = "";
     for (var i=0;i<global_world_params.creatures.length;i++) {
-        text += "creature ["+i+"]: count="+creaturesCount[i]+"\n";
+        text += "creature ["+(i+1)+"]: count="+creaturesCount[i]+"\n";
     }
     $(textbox).text(text);
 }
