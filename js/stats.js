726725e23dbd1b238c21ac78cfbf784daa8b6420
Yaniv Ben-Yehuda <yanivby@artwaresoft.com>
2018-12-19 12:18:11 +0200

    Calc stats for distribution of sizes

--- js/stats.js
+++ js/stats.js
@@ -26,11 +26,37 @@ SamplingGroup.prototype.avg = function()
     return (this.sum) ? (this.sum / this.count).toFixed(2) : '';
 }
 
+function SamplingEnum()
+{
+    this.vals = {};
+    this.count = 0;
+}
+SamplingEnum.prototype.sample = function(val)
+{
+    this.count++;
+    if (!this.vals[val]) this.vals[val] = 1;
+    else this.vals[val]++;
+}
+SamplingEnum.prototype.valsByFreq = function()
+{
+    var vals = [];
+    for (val in this.vals) {
+        vals.push({val: val, freq: this.vals[val]})
+    }
+    vals.sort(function(a,b) { return b.freq-a.freq });
+    return vals;
+}
+SamplingEnum.prototype.toString = function()
+{
+    return this.valsByFreq().map(x => x.val + ': ' + x.freq).join(', ');
+}
+
 stats = {
     calcStats: function(world) {
         var statsObj = { cycle: world.currentCycle | 0 };
 
         statsObj.vegetation = new SamplingGroup(worldParams.veg.maxAmount);
+        statsObj.sizes = new SamplingEnum();
         statsObj.creatures = [];
         for (var i=0;i<worldParams.creatures.length;i++) {
             statsObj.creatures.push(new SamplingGroup(worldParams.creature["l"].maxHealth));
@@ -40,7 +66,10 @@ stats = {
             for (var col=0;col<world.size;col++) {
                 var cell = world.matrix[row][col];
                 statsObj.vegetation.sample(cell.vegetation);
-                if (cell.creature) statsObj.creatures[cell.creature.type].sample(cell.creature.health);
+                if (cell.creature) {
+                    statsObj.creatures[cell.creature.type].sample(cell.creature.health);
+                    statsObj.sizes.sample(cell.creature.size);
+                }
             }
         }
 
@@ -53,6 +82,7 @@ stats = {
                     + ', health: '+ statsObj.creatures[i].avg() +"\n";
         }
         text += "vegetation: " + statsObj.vegetation.avg() + "\n";
+        text += "sizes: " + statsObj.sizes.toString() + "\n";
         text += "cycle: " + utils.numberWithCommas(statsObj.cycle);
         return text;
     }
