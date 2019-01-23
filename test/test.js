da1e0e766fd46006dafff9128f94cd0ece37925e
Yaniv Ben-Yehuda <yanivby@artwaresoft.com>
2019-01-23 14:12:43 +0200

    Fixes for tests

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ var massert = require('assert');
 function creature1Params()
 {
     return {
-        size: "s",
+        size: 0,
         actions: [
             { t: 'eat', p: 100 },
             { t: 'breed', p: 33, minHealth: 5 },
@@ -26,7 +26,7 @@ function creature1Params()
 function creature2Params()
 {
     return {
-        size: "m",
+        size: 1,
         actions: [
             { t: 'breed', p: 12, minHealth: 5 },
             { t: 'eat', p: 80 },
@@ -35,12 +35,12 @@ function creature2Params()
     }
 }
 
-describe('Life', function() {
+describe('Life', () => {
     var myworld = new world.World();
     myworld.init(50);
     myworld.addCreatures();
     var statStr = stats.calcStats(myworld).toString();
-    it('stat checking, cycle 0', function() {
+    it('stat checking, cycle 0', () => {
         massert(statStr.indexOf('red') > -1);
         massert(statStr.indexOf('health') > -1);
         massert(statStr.indexOf('vegetation') > -1);
@@ -50,7 +50,7 @@ describe('Life', function() {
 
     myworld.cycle();
     var statStr2 = stats.calcStats(myworld).toString();
-    it('stat checking, cycle 1', function() {
+    it('stat checking, cycle 1', () => {
         massert(statStr2.indexOf('red') > -1);
         massert(statStr2.indexOf('health') > -1);
         massert(statStr2.indexOf('vegetation') > -1);
@@ -58,7 +58,7 @@ describe('Life', function() {
     });
 
     var creature = myworld.findCreature();
-    it('creature params', function() {
+    it('creature params', () => {
         massert(creature.health > 0);
         massert(!!creature.logic.params.actions.length);
         massert(!!creature.logic.params.name);
@@ -70,8 +70,8 @@ describe('Life', function() {
     mydna.fromCreatureParams(params);
     var params2 = mydna.toCreatureParams();
 
-    it('creature dna serialization-desrialization', function() {
-        massert.equal(mydna.buffer.byteLength, 7);
+    it('creature dna serialization-desrialization', () => {
+        massert.equal(mydna.arr.length, 7);
         massert.equal(params.actions.size, params2.actions.size);
         for (var i=0;i<params.actions.size;i++)
             massert.equal(params.actions[i].t, params2.actions[i].t);
@@ -81,7 +81,7 @@ describe('Life', function() {
     var babyParams = dna.creatureParamsForBaby(creature1Params(), creature2Params(), false);
     var babyParamsMut = dna.creatureParamsForBaby(creature1Params(), creature2Params(), true);
 
-    it('breeding creatures', function() {
+    it('breeding creatures', () => {
         massert(babyParams.actions.length > 0);
         massert(babyParamsMut.actions.length > 0);
         massert(babyParams.actions[0].p !== undefined);
