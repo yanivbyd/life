f8998f900acd93ff49e402f2ceb6ca5f69bc4917
yanivbyd <yanivby@artwaresoft.com>
2024-03-09 16:29:43 +0200

    Rain map

--- ts/renderCanvas.ts
+++ ts/renderCanvas.ts
@@ -5,11 +5,13 @@ import { assertNotNull } from './utils/assert.js';
 import { RGB } from './rgb.js';
 
 export class CanvasRenderer {
+    displayCreatures: boolean;
     canvas: Object;
     width: number;
     height: number;
     world: World;
     vegRgbValues: RGB[];
+    rainRgbValues: RGB[];
     maxVegForRgbValue: number;
 
     constructor(canvas: Object, world: World) {
@@ -18,12 +20,19 @@ export class CanvasRenderer {
         this.width = Math.floor($(canvas).width());
         this.height = Math.floor($(canvas).height());
         this.maxVegForRgbValue = 0;
+        this.displayCreatures = true;
         this._initVegColors();
+        this._initRainColors();
 
         assertEquals(this.width, this.world.width);
         assertEquals(this.height, this.world.height);
     }
 
+    toggleDisplay(): boolean {
+        this.displayCreatures = !this.displayCreatures;
+        this.render();
+        return this.displayCreatures;
+    }
     private _initVegColors() {
         const startColor: RGB = new RGB(255, 255, 255);
         const endColor: RGB = new RGB(38, 255, 0);
@@ -41,6 +50,21 @@ export class CanvasRenderer {
         }
     }
 
+    private _initRainColors() {
+        const startColor: RGB = new RGB(255, 255, 255);
+        const endColor: RGB = new RGB(38, 255, 0);
+        const maxRain: number = 10;
+
+        this.rainRgbValues = [];
+        for (let i=0;i<=maxRain;i++) {
+            this.rainRgbValues.push({
+                red: this._relativeVal(startColor.red, endColor.red, maxRain, i),
+                green: this._relativeVal(startColor.green, endColor.green, maxRain, i),
+                blue: this._relativeVal(startColor.blue, endColor.blue, maxRain, i)
+            });
+        }
+    }
+
     private _relativeVal(start: number, end: number, maxVal: number, val: number): number {
         return Math.floor(start + (end-start) * (val/maxVal));
     }
@@ -54,6 +78,10 @@ export class CanvasRenderer {
     }
 
     render(): void {
+        if (!this.displayCreatures) {
+            this.renderRainMap();
+            return;
+        }
         var ctx = ($(this.canvas)[0] as HTMLCanvasElement).getContext('2d');
         var imageData = ctx.createImageData(this.width, this.height);
         let dataIndex = 0;
@@ -67,6 +95,23 @@ export class CanvasRenderer {
                 if (cell.creature) {
                     color = cell.creature.getTypeDef().color;
                 }
+                assertNotNull(color);
+                dataIndex = this._renderPixel(imageData.data, color, dataIndex);
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    renderRainMap() {
+        var ctx = ($(this.canvas)[0] as HTMLCanvasElement).getContext('2d');
+        var imageData = ctx.createImageData(this.width, this.height);
+        let dataIndex = 0;
+
+
+        for (var j = 0; j < this.height; j++) {
+            for (var i = 0; i < this.width; i++) {
+                var cell = this.world.matrix[i][j];
+                var color = this.rainRgbValues[Math.min(cell.rain + this.world.rainDelta, 10)];
                 assertNotNull(color);
                 dataIndex = this._renderPixel(imageData.data, color, dataIndex);
             }
